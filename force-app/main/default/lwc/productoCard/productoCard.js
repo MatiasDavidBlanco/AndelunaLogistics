79b1d7e41548b8e165e8546318aa646b1f64c4bd
Matias Blanco <matias.david.blanco@gmail.com>
2023-07-27 17:14:26 -0300

    Trabajando en componentes

--- force-app/main/default/lwc/productoCard/productoCard.js
+++ force-app/main/default/lwc/productoCard/productoCard.js
@@ -1,3 +1,7 @@
-import { LightningElement } from 'lwc';
+// Creación de la variable producto que va a ser llenada a partir de la información pasada desde el componente padre
 
-export default class ProductoCard extends LightningElement {}
+import { LightningElement, api } from 'lwc';
+
+export default class ProductoCard extends LightningElement {
+    @api producto;
+} 
--- force-app/main/default/lwc/productosLista/productosLista.js
+++ force-app/main/default/lwc/productosLista/productosLista.js
@@ -1,8 +1,10 @@
+// Conexion de l método getAllProductos al componente ProductosLista 
+
 import { LightningElement, wire} from 'lwc';
 import getAllProductos from "@salesforce/apex/ProductosService.getAllProductos";
 
 export default class ProductosLista extends LightningElement {
     
-    @wire(getAllProductos)
-    productos;
+    @wire(getAllProductos) ProductosLista;
+    
 }
