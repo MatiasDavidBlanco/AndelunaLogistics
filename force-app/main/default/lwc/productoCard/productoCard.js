f2c4b2d3bf4ba770eb18b31ed7dbd991818f5b59
Matias Blanco <matias.david.blanco@gmail.com>
2023-08-01 12:14:53 -0300

    Añadiendo boton ver detalles a producto card LWC

--- force-app/main/default/lwc/productoCard/productoCard.js
+++ force-app/main/default/lwc/productoCard/productoCard.js
@@ -1,7 +1,21 @@
 // Creación de la variable producto que va a ser llenada a partir de la información pasada desde el componente padre
 
 import { LightningElement, api } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class ProductoCard extends LightningElement {
+export default class ProductoCard extends NavigationMixin (LightningElement) {
     @api producto;
+
+    navigateToRecordViewPage() {
+        // View a custom object record.
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: this.producto.Id,
+                objectApiName: 'Producto__c', // objectApiName is optional
+                actionName: 'view'
+            }
+        });
+    }
+
 } 
