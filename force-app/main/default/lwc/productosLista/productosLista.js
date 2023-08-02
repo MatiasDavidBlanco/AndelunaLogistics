713522857de4f4bbb9411db48495e2224ebe540d
Matias Blanco <matias.david.blanco@gmail.com>
2023-08-02 12:14:53 -0300

    Creando input de busqueda en productos explorer

--- force-app/main/default/lwc/productosLista/productosLista.js
+++ force-app/main/default/lwc/productosLista/productosLista.js
@@ -2,9 +2,27 @@
 
 import { LightningElement, wire} from 'lwc';
 import getAllProductos from "@salesforce/apex/ProductosService.getAllProductos";
+import getProductoFiltrados from "@salesforce/apex/ProductosService.getProductoFiltrados";
 
 export default class ProductosLista extends LightningElement {
-    
+
+    //Properties Getters and Setters
+    searchText;
+    //Lifecycle Hooks
+
+    // Wires
     @wire(getAllProductos) ProductosLista;
-    
+    @wire(getProductoFiltrados, { searchText: "$searchText" })
+
+    // Methods 
+
+    handleInputChange(event){
+        const searchTextAux = event.detail.value;
+        if (searchTextAux.length >= 2 || searchTextAux === ''){
+            this.searchText = searchTextAux;
+        }
+        }
 }
+
+
+//llegue  a minuto 30 video 10
